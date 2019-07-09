300eeb0f477c4163e2511125986043f36a372f8a
Alberto Lacerda <albertoflfilho@gmail.com>
2019-07-09 16:14:52 -0400

    express using req.query

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send('myInstagram');
+  return res.send(`myInstagram ${req.query.name}`);
 });
 
 app.listen(3333);
