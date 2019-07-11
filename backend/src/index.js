3bd8bca13e382d7d6b102cd0b84e2214f527640d
Alberto Lacerda <albertoflfilho@gmail.com>
2019-07-11 12:16:05 -0400

    router

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send(`myInstagram ${req.query.name}`);
-});
+mongoose.connect(
+  'mongodb+srv://albertoflfilho:dbUserPassword@cluster0-iz5hy.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
+
+app.use(require('./routes'));
 
 app.listen(3333);
