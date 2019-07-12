e093b13156a0f7a17d672f145f3dd445c0f117c2
Alberto Lacerda <albertoflfilho@gmail.com>
2019-07-12 11:52:52 -0400

    cors configured

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,8 @@ mongoose.connect(
   'mongodb+srv://albertoflfilho:dbUserPassword@cluster0-iz5hy.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true }
 );
+
+app.use(cors());
 
 app.use(
   '/files',
