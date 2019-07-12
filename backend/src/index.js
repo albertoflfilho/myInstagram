d5431cf6fc540d038e77b109aa8b701a72c6edd0
Alberto Lacerda <albertoflfilho@gmail.com>
2019-07-12 11:48:34 -0400

    router /file/fileName.jpg implementedand saving in a .jpeg

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect(
   'mongodb+srv://albertoflfilho:dbUserPassword@cluster0-iz5hy.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true }
+);
+
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
 );
 
 app.use(require('./routes'));
